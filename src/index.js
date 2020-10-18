262298e93adc469499682e5bb794dc9931273d1d
ekusuy <30982583+ekusuy@users.noreply.github.com>
2020-10-18 16:16:22 +0900

    削除の実装

--- src/index.js
+++ src/index.js
@@ -13,11 +13,28 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // 完了ボタンを追加
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // 削除ボタンを追加
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
   // divタグの子要素に要素を追加
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了のリストに追加
-  document.getElementById("imcomplete-list").appendChild(div);
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
